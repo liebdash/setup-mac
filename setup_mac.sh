9efa44fdc703f6ffce3e7e36db6ad2ddbf48e170
Justin Lieb <84800426+liebdash@users.noreply.github.com>
2024-07-22 14:15:45 -0400

    new stuff

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,25 +1,50 @@
-# !/bin/bash
+#!/bin/bash
 
-# dev tools
-brew install bat 
+# Install Formulae
+brew install bash
+brew install bat
+brew install buf
+brew install coreutils
 brew install defaultbrowser
 brew tap jakehilborn/jakehilborn && brew install displayplacer
 brew install fd
 brew install fzf
-# - sign in with key
 brew install gh
+brew install git
+brew install go
+brew install grep
 brew install grpcurl
-brew install helm
-brew install hub
 brew install jq
-# - alias to k
-brew install kubectl
-brew install kubectx
 brew install k9s
+brew install krb5
+brew install neovim
 brew install node
+brew install pipenv
+brew install pycparser
+brew install python@3.10
+brew install python@3.11
+brew install python@3.12
 brew install ripgrep
-brew install tfenv
-brew install tig
+brew install virtualenv
+brew install yarn
+
+# Install Casks
+brew install --cask 1password
+brew install --cask 1password-cli
+brew install --cask bloomrpc
+brew install --cask brave-browser
+brew install --cask corretto11
+brew install --cask corretto17
+brew install --cask corretto@11
+brew install --cask corretto@17
+brew install --cask docker
+brew install --cask intellij-idea
+brew install --cask notion
+brew install --cask scroll-reverser
+brew install --cask spotify
+brew install --cask visual-studio-code
+brew install --cask warp
+brew install --cask zoom
 
 
 # desktop apps
@@ -28,17 +53,39 @@ brew install --cask arc
 # - override cmd + space
 # - turn off side bar
 # - start at login
+brew install --cask chatgpt
+brew install --cask cold-turkey-blocker
 brew install --cask contexts
-# - start at login
+brew install --cask docker
+brew install --cask obsidian
+brew install --cask raycast
 brew install --cask scroll-reverser 
 # - start at login
 brew install --cask spotify 
 # - set hotkey
 brew install --cask warp 
+brew install --cask visual-studio-code
+brew install --cask vivid
 brew install --cask zoom
 
 # dev environment apps
 if [ "$1" == "--doordash" ]; then
+    brew install cadence-workflow
+    brew install helm
+    brew install hub
+    brew install jenv
+    brew install kcat
+    brew install kubectx
+    brew install kubernetes-cli
+    brew install okta-aws-tools
+    brew install postgresql@14
+    brew install pre-commit
+    brew install pyenv
+    brew install terraform
+    brew install tfenv
+    brew install vault
+
+
     brew install --cask intellij-idea
     brew install --cask pycharm
 
@@ -53,7 +100,18 @@ else
     sudo xattr -dr com.apple.quarantine /Applications/IntelliJ\ IDEA\ CE.app/ && sudo chmod -R 777 /Applications/IntelliJ\ IDEA\ CE.app/
     sudo xattr -dr com.apple.quarantine /Applications/PyCharm\ CE.app/ && sudo chmod -R 777 /Applications/PyCharm\ CE.app/ 
 fi
-brew install --cask visual-studio-code
+
+
+
+
+
+
+
+# ./vimrc -> ~/.vimrc
+
+
+
+
 
 
 # set arc as default browser
